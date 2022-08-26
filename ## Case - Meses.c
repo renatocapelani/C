5d4054226ee6d2c0cb1a4adaacde490f040423a7
renatocapelani <renato.capelani@hotmail.com>
2022-08-26 15:45:54 -0300

    first commit

---
+++ ## Case - Meses.c
@@ -0,0 +1,15 @@
+Teste 001
+git --version
+git config --global user.name "renatocapelani"
+git config --global user.email "renato.capelani@hotmail.com"
+git commit -m "Primeiro Commit"
+git init
+git add index.php
+git commit -m "Primeiro Commit"
+echo "# PHP" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/renatocapelani/PHP.git
+git push -u origin main
